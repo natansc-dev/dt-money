b4a1f02f03efeadb38ff094e8d1ae58df3ef1993
Natan Cardoso <natansc73@gmail.com>
2022-11-15 21:37:48 -0300

    memo in component SearchForm

--- src/pages/Transactions/components/SeachForm/index.tsx
+++ src/pages/Transactions/components/SeachForm/index.tsx
@@ -3,8 +3,9 @@ import { MagnifyingGlass } from 'phosphor-react'
 import { useForm } from 'react-hook-form'
 import * as z from 'zod'
 import { zodResolver } from '@hookform/resolvers/zod'
-import { useContext } from 'react'
 import { TransactionsContext } from '../../../../context/TransactionsContext'
+import { useContextSelector } from 'use-context-selector'
+// import { memo } from 'react'
 
 const searchFormSchema = z.object({
   query: z.string(),
@@ -13,7 +14,12 @@ const searchFormSchema = z.object({
 type SearchFormInputs = z.infer<typeof searchFormSchema>
 
 export function SearchForm() {
-  const { fetchTransactions } = useContext(TransactionsContext)
+  const fetchTransactions = useContextSelector(
+    TransactionsContext,
+    (context) => {
+      return context.fetchTransactions
+    },
+  )
   const {
     register,
     handleSubmit,
@@ -40,3 +46,5 @@ export function SearchForm() {
     </SearchFormContainer>
   )
 }
+
+// export const SearchForm = memo(SearchFormComponent)
